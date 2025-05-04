da4912a411a7e474a8361f5842dde88170986ed0
vishal kumar <vishalkumar150206@gmail.com>
2025-05-04 07:49:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let popUp = document.getElementById("popUp");
+
+function openpopUp() {
+    popUp.classList.add("open-popUp");
+}
+
+function closepopUp() {
+    popUp.classList.remove("open-popUp");
+}
